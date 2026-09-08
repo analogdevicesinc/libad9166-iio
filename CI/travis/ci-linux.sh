a223a634c5a32f0fad9ceb6eef134093074e8c0e
Alin Opris <alin.opris@analog.com>
2026-09-08 15:00:37 +0300

    ci:added actions for azure migration 	- added build for multiple distributions of linux 	- added build for arm 	- added build for windows 	- added build for macOS  Signed-off-by: Alin Opris <alin.opris@analog.com>

--- CI/travis/ci-linux.sh
+++ CI/travis/ci-linux.sh
@@ -11,6 +11,14 @@ handle_centos() {
 
 }
 
+handle_fedora() {
+	local package=$1
+	dnf install -y ./$package
+	export LD_LIBRARY_PATH=/usr/local/lib64/
+	export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
+	export CMAKE_OPTIONS="-DPYTHON_BINDINGS=ON -DENABLE_PACKAGING=ON .."
+}
+
 handle_default() {
 	local package=$1
 	DEBIAN_FRONTEND=noninteractive apt-get install -y rpm
